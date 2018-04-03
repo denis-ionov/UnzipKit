f6e42f588630c6880330cb28915ed2c271a0f760
Dov Frankel <dov@abbey-code.com>
2018-04-03 08:27:51 -0400

    Tried setting the OS_ACTIVITY_MODE environment variable from my CocoaPods test script rather than in a scheme that the test spec probably doesn't use at all

--- Scripts/cocoapod-validate.sh
+++ Scripts/cocoapod-validate.sh
@@ -8,11 +8,7 @@ pod --version
 
 pod env
 
-# Make the test logging output quieter
-if [ -n "$CI" ]; then
-    echo -e "\nSetting log level to default for CI build..."
-    sudo log config --mode "level:default"
-fi
+export "OS_ACTIVITY_MODE"="disable"
 
 # Lint the podspec to check for errors. Don't call `pod spec lint`, because we want it to evaluate locally
 pod lib lint --verbose
