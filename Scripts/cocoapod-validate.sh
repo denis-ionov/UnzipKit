3065140dcd9f462c8222146a646f033316ef5f5c
Dov Frankel <dov@abbey-code.com>
2018-04-04 17:08:48 -0400

    Trying an approach to remove the buffering pipes usually perform, so the output doesn't hang, tripping up Travis

--- Scripts/cocoapod-validate.sh
+++ Scripts/cocoapod-validate.sh
@@ -4,14 +4,12 @@ set -ev
 
 . Scripts/set-travis-tag-to-latest.sh
 
-pod --version
-
 pod env
 
 # Lint the podspec to check for errors. Don't call `pod spec lint`, because we want it to evaluate locally
 
 # Using sed to remove logging from output until CocoaPods issue #7577 is implemented and I can use the
 # OS_ACTIVITY_MODE = disable environment variable from the test spec scheme
-pod lib lint --verbose | sed '/xctest/d; /^$/d'
+script -q /dev/null pod lib lint --verbose | sed '/xctest/d; /^$/d'
 
 . Scripts/unset-travis-tag.sh
