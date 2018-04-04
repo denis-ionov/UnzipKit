715367c838f45ea2d6fee2034ee43cfa1ae1b0d1
Dov Frankel <dov@abbey-code.com>
2018-04-04 08:45:01 -0400

    Updated pod lib lint command to filter out log lines

--- Scripts/cocoapod-validate.sh
+++ Scripts/cocoapod-validate.sh
@@ -8,9 +8,10 @@ pod --version
 
 pod env
 
-export "OS_ACTIVITY_MODE"="disable"
-
 # Lint the podspec to check for errors. Don't call `pod spec lint`, because we want it to evaluate locally
-pod lib lint --verbose
+
+# Using sed to remove logging from output until CocoaPods issue #7577 is implemented and I can use the
+# OS_ACTIVITY_MODE = disable environment variable from the test spec scheme
+pod lib lint --verbose | sed '/xctest/d; /^$/d'
 
 . Scripts/unset-travis-tag.sh
