3ff2f35efe6082e77141128d5ac37378f418f82b
Dov Frankel <dov@abbey-code.com>
2018-04-04 17:40:20 -0400

    Trying awk instead of sed

--- Scripts/cocoapod-validate.sh
+++ Scripts/cocoapod-validate.sh
@@ -8,8 +8,8 @@ pod env
 
 # Lint the podspec to check for errors. Don't call `pod spec lint`, because we want it to evaluate locally
 
-# Using sed to remove logging from output until CocoaPods issue #7577 is implemented and I can use the
+# Using awk to remove logging from output until CocoaPods issue #7577 is implemented and I can use the
 # OS_ACTIVITY_MODE = disable environment variable from the test spec scheme
-script -q /dev/null pod lib lint --verbose | sed '/xctest/d; /^$/d'
+pod lib lint --verbose | awk '$0 !~ /xctest\[/ && $0 !~ /^$/ {print}'
 
 . Scripts/unset-travis-tag.sh
