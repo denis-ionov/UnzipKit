6fc0ac2c877eb30f180bd21cd5d9c82455f758c2
Dov Frankel <dov@abbey-code.com>
2018-04-05 18:01:05 -0400

    Applying wait to pod lib lint so Travis doesn't time out

--- Scripts/cocoapod-validate.sh
+++ Scripts/cocoapod-validate.sh
@@ -10,6 +10,6 @@ pod env
 
 # Using awk to remove logging from output until CocoaPods issue #7577 is implemented and I can use the
 # OS_ACTIVITY_MODE = disable environment variable from the test spec scheme
-pod lib lint --verbose | gstdbuf -i0 -o0 sed '/xctest\[/d; /^$/d'
+travis_wait 15 pod lib lint --verbose | sed '/xctest\[/d; /^$/d'
 
 . Scripts/unset-travis-tag.sh
